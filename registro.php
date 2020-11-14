774e51809aacf0fbcf962179bd6860b65c86289f
eddy-etec <74433587+eddy-etec@users.noreply.github.com>
2020-11-13 22:14:18 -0600

    Update registro.php

--- registro.php
+++ registro.php
@@ -1,8 +1,6 @@
 <?php
   include ("conexion/Conexion.php");
-  //include ("Encryptar.php");
   $bd = new Conexion();
-  //$enc = new Encryptar();
   session_start();
   if(isset($_SESSION["id_usuario"])){
     header("Location: index.php");
@@ -38,20 +36,13 @@
     <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
 </head>
 
 <body>
 
     <?php
       if(isset($_POST["registro"])){
-        //echo "<script>alert('Entrar');</script>";
+        
 
         $nombre = $_POST["nombre"];
         $primer_apellido = $_POST["primer_apellido"];
